421d97356bafccece61114738b01e3a414c03113
LeandroPalma27 <psychomusicbox@gmail.com>
2024-01-05 22:08:15 -0500

    Error de compilador corregido

--- src/components/CCState.tsx
+++ src/components/CCState.tsx
@@ -2,26 +2,30 @@ import { string } from "prop-types";
 import React, { Component } from "react";
 
 type CCStateProps = {
-  apellido?: string
+  nombre?: string,
+  apellido?: string,
+  value?: number
 }
 
 export default class CCState extends Component<CCStateProps> {
+  state = {
+    value: 0,
+    nombre: 'Leandro'
+  }
+
   constructor(props) {
     super(props);
-    console.log(props);
-    this.state = {
-      nombre: 'Leandro',
-      value: 0
-    };
   }
+
   addNumber = () => {
     this.setState({value: this.state.value + 1});
   }
+
   render() {
     return <>
     <h3>{this.state.nombre}</h3>
       <div>{this.state.value}</div>
-      <button onClick={this.addNumber}>Click</button>
+      <button onClick={this.addNumber}>+1 (CLASS COMPONENT)</button>
     </>;
   }
 }

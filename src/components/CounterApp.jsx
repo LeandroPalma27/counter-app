fe2dffef614df2f1bc1e84322c55adb7cd5031fd
LeandroPalma27 <psychomusicbox@gmail.com>
2024-01-06 17:26:11 -0500

    Funciones para reducir el valor y resetearlos, estan hechas (SE MANEJA ESTADO Y EL USE DEL SETCOUNTER)

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+const [counter, setState] = useState(value);
+
+const handleValue = () => {
+  setState(counter + 1);
+};
+
+const handleSubstract = () => {
+  setState(counter -1);
+}
+
+const handleReset = () => {
+  setState(0);
+}
+
 export const CounterApp = ({ value }) => {
-
-  const [counter, setState] = useState(value);
-
-  const handleValue = () => {
-    setState(counter + 1);
-  };
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
       <button onClick={handleValue}>+1 (FUNCTIONAL COMPONENT)</button>
+      <button onClick={handleSubstract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
